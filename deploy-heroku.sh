5d93611568d1e2cae80763fd460df1c3b94931ce
YunYouJun <me@yunyoujun.cn>
2018-05-24 17:29:20 +0800

    use param by heroku bash

---
+++ deploy-heroku.sh
@@ -0,0 +1,4 @@
+$TOKEN=$1
+export NODE_ENV='production'
+export PROD_TOKEN=$TOKEN
+npm run build
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,14 +1,9 @@
 import env from '../env'
 
-let TOKEN = process.env.NODE_ENV === 'production' ? process.env.PROD_TOKEN : env.LOCAL_TOKEN
-// heroku
-if (process.env.NODE_API_TOKEN) {
-  console.log(process.env.NODE_API_TOKEN)
-  TOKEN = process.env.NODE_API_TOKEN
-}
+let TOKEN = 'COC_API_TOKEN'
+TOKEN = process.env.NODE_ENV === 'production' ? process.env.PROD_TOKEN : env.LOCAL_TOKEN
 
 export default function ({ $axios, redirect }) {
-  console.log(TOKEN)
   $axios.setToken(TOKEN, 'Bearer')
   
   $axios.onRequest(config => {
