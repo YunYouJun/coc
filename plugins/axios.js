58409057969ed62fc8de5f7d1802652566f17067
YunYouJun <me@yunyoujun.cn>
2018-05-24 15:48:34 +0800

    nginx conf & env.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,12 @@
-const LOCAL_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk5MDQ5NzI1LTViZGEtNDk3YS1hNjAwLTRkM2Q4NmFkZWM5MyIsImlhdCI6MTUyNzEyOTgyNywic3ViIjoiZGV2ZWxvcGVyLzBlNmQ4YjRiLTY1YTktMTVlZS0yNTFlLTliYzMxNGFhM2Y0NSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEyNC43Ni4xMDguMzIiXSwidHlwZSI6ImNsaWVudCJ9XX0.VD6oOJANBYZ8L_GW-OyYuIIirHeiLCQwgP6cyRYokJTpnWmH_1oy2ElRlg7TarUe-cOXmZyBhGvTrbCm8zkQpQ'
+import env from '../env'
 
-const PROD_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZjMmJhMGQ0LWIxYzEtNDNkNC05YjhlLWVhYzMxNjE5ZThlYiIsImlhdCI6MTUyNjkwNzU1Nywic3ViIjoiZGV2ZWxvcGVyLzBlNmQ4YjRiLTY1YTktMTVlZS0yNTFlLTliYzMxNGFhM2Y0NSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzOS4xOTkuMjIuOTAiXSwidHlwZSI6ImNsaWVudCJ9XX0.Ry8xiD2kwW5ATJdHV7UyQUNfe5pKP-zBigi8Ho76DvhZRAhc7uTh3mamfw0O5cRSbd3gV71WKwHc_BDvTyGKIA'
-
-let TOKEN = process.env.NODE_ENV === 'production' ? PROD_TOKEN : LOCAL_TOKEN
-
-if (process.env.NODE_ENV === 'heroku' && process.env.TOKEN) {
+let TOKEN = process.env.NODE_ENV === 'production' ? env.PROD_TOKEN : env.LOCAL_TOKEN
+// heroku
+if (process.env.TOKEN) {
   TOKEN = process.env.TOKEN
 }
 
 export default function ({ $axios, redirect }) {
-  console.log(process.env.NODE_ENV, TOKEN)
   $axios.setToken(TOKEN, 'Bearer')
   
   $axios.onRequest(config => {
