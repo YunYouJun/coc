b50c358deaf23106f3a261b93e208e4f3e0c57e6
YunYouJun <me@yunyoujun.cn>
2018-05-26 15:04:48 +0800

    cross-env

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -5,4 +5,4 @@ git reset --hard
 git pull
 export NODE_ENV='production'
 echo "module.exports = {PROD_TOKEN:'$TOKEN'}" > env.js
-yarn && npm run build && pm2 restart coc || pm2 start npm --name "coc" -- run start
+yarn install && npm run build && pm2 restart coc || pm2 start npm --name "coc" -- run start
